2cfdd1413f014549af8fc1c06118ddfdf49f0be2
manar khemakhem <manarkhemakhem0@gmail.com>
2025-03-24 12:43:31 +0100

    modification design

--- src/app/group/group.component.ts
+++ src/app/group/group.component.ts
@@ -19,6 +19,10 @@ export class GroupComponent implements OnInit, AfterViewInit {
   totalGroups: number = 0;
   collaboratorsCountByGroup: { groupLabel: string, count: number }[] = [];  // Remplacer `groupId` par `groupLabel`
   errorMessage: string = '';  // Déclaration de errorMessage pour gérer les erreurs
+  currentPage: number = 1;  // Page actuelle
+  itemsPerPage: number = 5;  // Nombre d'éléments par page
+  totalPages: number = 1;  // Nombre total de pages
+  paginatedGroups: Group[] = [];  // Liste des groupes à afficher pour la page actuelle
 
   constructor(
     private groupService: GroupService,
@@ -31,6 +35,8 @@ export class GroupComponent implements OnInit, AfterViewInit {
       (data) => {
         this.groups = data;
         this.totalGroups = data.length;
+        this.totalPages = Math.ceil(this.totalGroups / this.itemsPerPage);
+        this.paginateData();
         // Charger les collaborateurs pour chaque groupe
       },
       (error) => {
@@ -43,5 +49,26 @@ export class GroupComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
 
   }
-  
+  // Méthode pour gérer la pagination
+  paginateData(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedGroups = this.groups.slice(startIndex, endIndex);
+  }
+
+  // Méthode pour aller à la page suivante
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.paginateData();
+    }
+  }
+
+  // Méthode pour revenir à la page précédente
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.paginateData();
+    }
+  }
 }
